101fbfc5bf82b49b8da89d5366f7645a7a753758
Czechthisout <ajcech23@gmail.com>
2023-04-08 19:58:45 -0700

    added flare to the button design

--- components/rock_paper_scissors.js
+++ components/rock_paper_scissors.js
@@ -2,6 +2,19 @@ let computerSelection;
 let playerSelection;
 let computerScore = 0;
 let playerScore = 0;
+const result = document.createElement('div');
+document.body.appendChild(result);
+
+document.getElementsByClassName('rock')[0].addEventListener('click', (event) => {event.target.classList.add('button-clicked'); playRound('rock', getComputerChoice())});
+document.getElementsByClassName('paper')[0].addEventListener('click', (event) => {event.target.classList.add('button-clicked'); playRound('paper', getComputerChoice())});
+document.getElementsByClassName('scissors')[0].addEventListener('click', (event) => {event.target.classList.add('button-clicked'); playRound('scissors', getComputerChoice())});
+
+let scoreBoard = document.createElement('p');
+document.body.appendChild(scoreBoard);
+
+function updateScoreBoard(){
+    scoreBoard.textContent = "Player Score: " + playerScore + "\n" + "Computer Score: " + computerScore;
+}
 
 let rps = ["rock", "paper", "scissors"];
 function getComputerChoice(){
@@ -10,50 +23,56 @@ function getComputerChoice(){
     return item;
 }
 
-//console.log(getComputerChoice());
-
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
     computerSelection=getComputerChoice();
 
     if (playerSelection === computerSelection){
-        return "It's a tie! Both players chose " + playerSelection + ".";
+        result.textContent = "It's a tie! Both players chose " + playerSelection + ".";
+        console.log(result);
+        playerSelection.classList.add('button-clicked');
     }
     else if(playerSelection == 'rock'){
         if(computerSelection == 'paper'){
             computerScore++;
-            return 'The computer won this round!';
+            result.textContent = 'The computer won this round!';
         }else{
             playerScore++;
-            return 'You won this round!';
+            result.textContent = 'You won this round!';
         }
     }
     else if(playerSelection == 'paper'){
         if(computerSelection == 'scissors'){
             computerScore++;
-            return 'The computer won this round!';
+            result.textContent = 'The computer won this round!';
         }else{
             playerScore++;
-            return 'You won this round!';
+            result.textContent = 'You won this round!';
         }
     }
     else if(playerSelection == 'scissors'){
         if(computerSelection == 'rock'){
             computerScore++;
-            return 'The computer won this round!';
+            result.textContent = 'The computer won this round!';
         }else{
             playerScore++;
-            return 'You won this round!';
+            result.textContent = 'You won this round!';
         }
     }
+    console.log(result);
+
+    updateScoreBoard();
 }
 
-function game(){
-    for (i=0; i<5;i++){
-        user_choice = prompt("input rock, paper, or scissors and see who comes out on top");
-        console.log(playRound(user_choice, getComputerChoice()));
-    }
-    console.log("Player scored ", playerScore);
-}
+//Refactored code commented out below from an old version of this game that relied on users' text input
 
-game();
+//function game_start(user_choice){
+    //for (i=0; i<5;i++){
+        // THIS IS NOW UI user_choice = prompt("input rock, paper, or scissors and see who comes out on top");
+    //console.log(playRound(user_choice, getComputerChoice()));
+    //}
+    //console.log("Player scored ", playerScore);
+//}
+
+//const buttons = Array.from(document.querySelectorAll('.button'));
+//buttons.forEach(button => buttons.addEventListener('transitionend', removeTransition));
+//window.addEventListener('keydown', playRound);
